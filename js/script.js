ad70e76c26d11dde62df95cbcbb1a3bad2040476
Saskia Theodora <saskiatheo137@gmail.com>
2024-07-05 01:22:17 +0700

    website

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // Ini Javascript
+
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let born = document.getElementById("born").value;
+  let male = document.getElementById("male").value;
+  let female = document.getElementById("female").value;
+  let message = document.getElementById("message").value;
+
+  console.log(name);
+  console.log(born);
+  console.log(male);
+  console.log(female);
+  console.log(message);
+
+  if (
+    (name && born && male && female && message != null) ||
+    (name && born && male && female && message != "")
+  ) {
+    alert("Pesan terkirim!");
+  } else {
+    alert("Silahkan input ulang data anda dengan benar!");
+  }
+}
